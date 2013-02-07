5c4cf04f11e78b82e648f80acf0179255db2eaf0
Magoa <you@example.com>
2013-02-07 05:14:23 +0000

    Modification des constante dans rsa_decrypt et devellopement de la fonction d'extraction du fichier claire

--- rsa_decrypt.c
+++ rsa_decrypt.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 
 #include "src/config.h"
 
@@ -14,34 +15,21 @@
 #include "src/usage_camellia.h"
 #include "src/camellia.h"
 #include "src/clee_pub.h"
+#include "src/sha4.h"
 
 int main( int argc, char *argv[] )
 {
+	printf("[i] Debut\n");
 	if(argc != 2)
 	{
 		printf("Usage: %s <fichier in> <fichier out>\n", argv[0]);
-		printf("Structure du fichier: <clee camellia><md5 originel (16)><IV (16)><Fichier>\n");
+		printf("Structure du fichier: <Cryptogramme (%d) (camellia (32) + sha512(64) + IV (16)) ><Fichier>\n", RSA_TAILLE/8);
 		return 0;
 	}
-	unsigned char IV[16] = {0}, IV_svg[16] = {0};
-	unsigned char md5[16] = {0};
-	unsigned char clee[32] = {0};
-	unsigned char cryptogramme_clee[RSA_TAILLE/8] = {0};
-	unsigned char *fichier = NULL, *fichier_chiffre = NULL;
+	unsigned char *fichier = NULL, *fichier_dechiffre = NULL;
 	FILE *p_fichier = NULL, *sortie = NULL;
 	camellia_context camellia;
 	unsigned int taille;
-	printf("[i] Initialisation des IV\n");
-	ret = 1;
-	srand(time(NULL));
-	for(ret = 0; ret < 16; ret++)
-	{
-		IV_svg[ret] = IV[ret] = rand();
-	}
-	ret = 1;
-	//Argument, le fichier
-	//Recuperer la clee camellia pour dechiffrer la clee rsa
-	//dechiffre la clee public
 	
 	//On map le fichier en memoire
 	printf("[i] Lecture de la taille du fichier %s\n", argv[1]);
@@ -55,13 +43,13 @@ int main( int argc, char *argv[] )
 	fseek(p_fichier, 0, SEEK_END);
 	taille = ftell(p_fichier);
 	rewind(p_fichier);
-	if((taille - RSA_TAILLE/8 - 16 - 166)%16 != 0)
+	if((taille - RSA_TAILLE/8)%16 != 0)
 	{
 		printf("[-] Erreur de taille. Pas de multiple de 16.\n");
 	} 	
 	printf("[i] Allocation de memoire pour les fichiers\n");
 	fichier = malloc(taille * sizeof(unsigned char));//On genere le fichier  
-	fichier_dechiffre = malloc((taille - RSA_TAILLE/8 - 16 - 16) * sizeof(unsigned char));
+	fichier_dechiffre = malloc((taille - RSA_TAILLE/8) * sizeof(unsigned char));
 	if(fichier == NULL || fichier_dechiffre == NULL)
 	{
 		printf("[-] Erreur d'allocation : %d octet(s)\n", taille * sizeof(unsigned char));
@@ -74,23 +62,30 @@ int main( int argc, char *argv[] )
 
 void dechiffrer(unsigned char *fichier_chiffre, int taille)
 {
-	unsigned char clee_camellia[32] = {0};
-	unsigned char IV[16] = {0};
-	unsigned char md5_origine[16] = {0};
-	unsigned char md5_claire[16] = {0};
-	unsigned char cryptogramme_camellia[RSA_TAILLE/8] = {0};
-	//Allouer un tableau de taille - 4096/8 -16 - 16 pour le fichier clair
-	unsigned char *fichier_claire = malloc(sizeof(unsigned char) * (taille - RSA_TAILLE/8 -16 -16));
+	unsigned char *clee_symetrique = NULL;
+	unsigned char *IV = NULL;
+	unsigned char *hash_origine = NULL;
+	unsigned char hash_claire[TAILLE_HASH/8] = {0};
+	unsigned char *cryptogramme_rsa = NULL;
+	unsigned char data_dechiffre[TAILLE_CLEE_RSA/8] = {0};
+	unsigned char *fichier = fichier_chiffre + TAILLE_CLEE_RSA/8;
+	camellia_context camellia;
+	//Allouer un tableau de taille- cryptogramme RSA pour le fichier clair
+	unsigned char *fichier_claire = malloc(sizeof(unsigned char) * (taille - RSA_TAILLE/8));
 	//recuperer cryptogramme clee camellia
-	
-	//recuperer md5
-	//recuperer IV
 	
 	//dechiffrer clee camellia
 	
+	//Extrait les données du texte claire
+	clee_symetrique = data_dechiffre;
+	hash_origine = data_dechiffre + TAILLE_CLEE_CAMELIA/8;
+	IV = data_dechiffre + TAILLE_CLEE_CAMELIA/8 + TAILLE_HASH/8;
+
 	//dechiffrer fichier
+	camellia_setkey_dec( &camellia, clee_symetrique, TAILLE_CLEE_CAMELIA);
+	camellia_crypt_cbc( &camellia, CAMELLIA_DECRYPT, taille, IV, fichier, fichier_claire);
 	
-	//calculer somme md5 du fichier claire
+	//calculer hash  du fichier claire
 	
-	//comparer les sommes md5
+	//comparer les hashs
 }
